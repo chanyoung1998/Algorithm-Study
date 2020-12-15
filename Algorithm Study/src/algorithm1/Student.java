2ffb231488e55af3cbc26dd08ac46533d7b0ae82
W21793 <W21793@172.41.182.29>
2020-12-15 20:34:21 +0900

    알고리즘 공부 선택정렬

---
+++ Algorithm Study/src/algorithm1/Student.java
@@ -0,0 +1,29 @@
+package algorithm1;
+
+public class Student {
+
+	private int score;
+	private String grade;
+	
+	public Student(int score) {
+		this.score = score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+	
+	
+	
+}
