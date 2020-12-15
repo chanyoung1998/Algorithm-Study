a55383d7b4f3489badbff5ac505225ede23d09dd
W21793 <W21793@172.41.182.29>
2020-12-15 20:38:18 +0900

    알고리즘 공부3 병합정렬

---
+++ Algorithm Study/src/alrgorithm3/Main.java
@@ -0,0 +1,14 @@
+package alrgorithm3;
+
+//문제:준성이가 컴퓨터 파일 정리를 하고 있다. 필요 없는 파일을 삭제하고 남은 파일을 순서대로 나열하고 싶다. 파일 이름이 숫자로 되어 있다.
+//문제제시:남은 파일을 병합정렬 알고리즘을 이용하여 오름차순으로 정렬하라
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
