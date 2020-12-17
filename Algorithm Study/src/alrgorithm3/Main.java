ffa8cc2f6f39fa91b62d7df5b030c7d17b22cb1b
W21793 <W21793@172.41.182.29>
2020-12-17 19:08:13 +0900

    algorithm공부3 mergeSort
    main함수 구현 및 MergeSort클래스 구현

--- Algorithm Study/src/alrgorithm3/Main.java
+++ Algorithm Study/src/alrgorithm3/Main.java
@@ -1,5 +1,7 @@
 package alrgorithm3;
 
+import java.util.Scanner;
+
 //문제:준성이가 컴퓨터 파일 정리를 하고 있다. 필요 없는 파일을 삭제하고 남은 파일을 순서대로 나열하고 싶다. 파일 이름이 숫자로 되어 있다.
 //문제제시:남은 파일을 병합정렬 알고리즘을 이용하여 오름차순으로 정렬하라
 
@@ -8,7 +10,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		
+		for(int i = 0 ; i < n ; i ++)
+		{
+			int input = sc.nextInt();
+			arr[i] = input;
+		}
+		
+		MergeSort.mergeSort(arr,n);
+		
+		for(int a : arr)
+			System.out.print(a + " ");
+			
+		
 	}
 
 }
