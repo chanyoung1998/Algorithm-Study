917b15fd0a1285eaba959f91e0c80651c9ee7de8
정찬영 <lms062111@naver.com>
2021-02-14 00:01:19 +0900

    21.2.13 그리디 알고리즘 마무리

--- 백준 문제풀기/파이썬/그리디/algorithm_1931.py
+++ 백준 문제풀기/파이썬/그리디/algorithm_1931.py
@@ -1,24 +1,21 @@
+'''
+내용:백준 알고리즘 단계별 풀기 그리디 마지막 문제
+날짜:21년2월13일
+사용 언어:파이썬
+'''
+
 n = int(input())
 time_tables = []
-count = 0
 for i in range(n):
-    time_tables.append(list(map(int,input().split())))
-time_tables.sort(key= lambda x: (x[0],x[1]))
+    time_tables.append(list(map(int, input().split())))
+time_tables.sort(key = lambda x:(x[1],x[0]))
 
-start = 0
-end = 0
-time = 0
-
-while i < len(time_tables):
-    for j in range(i+1,len(time_tables)):
-        if time_tables[j][0] <= time_tables[i][0]:
-            pass
-
-
-
-
-    start = time_tables[i][0]
-    end = time_tables[i][1]
-    time = end - start
-    i += 1
-
+start_time = time_tables[0][0]
+end_time = time_tables[0][1]
+count = 1
+for i in range(1, len(time_tables)):
+    if end_time <= time_tables[i][0]:
+        start_time = time_tables[i][0]
+        end_time = time_tables[i][1]
+        count += 1
+print(count)
