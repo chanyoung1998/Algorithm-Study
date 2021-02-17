4e223b045049421d76cb084c20274498672e86e6
정찬영 <lms062111@naver.com>
2021-02-17 16:54:45 +0900

    2.17  7단원 트랜잭션 생성 및 전파 실습

---
+++ 백준 문제풀기/파이썬/스택/algorithm_17298.py
@@ -0,0 +1,10 @@
+'''
+내용:백준 알고리즘 단계별 풀기 스택 17298 오큰수
+날짜:21년1월20일
+사용 언어:파이썬
+'''
+import sys
+
+n = int(sys.stdin.readline())
+a = list(sys.stdin.readline().rstrip().split(' '))
+
--- 백준 문제풀기/파이썬/스택/algorithm_1874.py
+++ 백준 문제풀기/파이썬/스택/algorithm_1874.py
@@ -3,4 +3,41 @@
 날짜:21년2월16일
 사용 언어:파이썬
 '''
+import sys
+n = int(sys.stdin.readline().rstrip())
+stack = []
+temp = []
+ret = []
+ret_string = ''
+index = 0
 
+for i in range(n):
+    stack.append(int(sys.stdin.readline().rstrip()))
+
+    while stack[index] in temp:
+        ret.append(temp.pop(-1))
+        ret_string += '-\n'
+        index += 1
+
+    temp.append(i+1)
+    ret_string += '+\n'
+
+ret_string += '-\n' * len(temp)
+ret += temp[::-1]
+
+'''for i in range(1,n+1):
+    while stack[index] in temp:
+        ret.append(temp.pop(-1))
+        ret_string.append('-')
+        index += 1
+    temp.append(i)
+    ret_string.append('+')
+'''
+'''while len(temp) > 0:
+    ret.append(temp.pop(-1))
+    ret_string.append('-')
+'''
+if ret == stack:
+    print(ret_string)
+else:
+    print('NO')
