cd56e7c13edd5e10f75152a02aff758feaa4d682
정찬영 <lms062111@naver.com>
2021-02-25 00:08:07 +0900

    21-2-25 6549번 try중,,

--- 백준 문제풀기/파이썬/분할 정복/algorithm_6549.py
+++ 백준 문제풀기/파이썬/분할 정복/algorithm_6549.py
@@ -1,11 +1,35 @@
-'''
-내용:백준 알고리즘 단계별 풀기 분할 정복 6549 히스토크램에서 가장 큰 직사각형
-날짜:21년2월24일
-사용 언어:파이썬
-'''
 import sys
 
+sys.setrecursionlimit(10 ** 6)
+
+
+def sol(array, start, end):
+    if start >= end:
+        return array[start]
+    mid_index = 0
+    min_value = min(array[start:end+1])
+    for i in range(start, end + 1):
+        if array[i] == min_value:
+            mid_index = i
+            break
+
+    right_maxsize = sol(array, start, mid_index - 1)
+    if mid_index == end:
+        left_maxsize = sol(array, mid_index, end)
+    else:
+        left_maxsize = sol(array, mid_index + 1, end)
+    mid_maxsize = min_value * (end - start + 1)
+
+    return max(right_maxsize, left_maxsize, mid_maxsize)
+
+
 while True:
-    inputs = list(map(int,sys.stdin.readline().split()))
+    inputs = list(map(int, sys.stdin.readline().rstrip().split()))
+
     if inputs == [0]:
         break
+    n = inputs[0]
+    array = inputs[1:]
+    print(sol(array, 0, n-1))
+
+
