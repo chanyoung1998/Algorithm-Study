c412409da94502678e9b47dd01409556c1517b64
정찬영 <lms062111@naver.com>
2021-04-10 23:33:58 +0900

    21.4.10 2206번 bfs풀이 ! 중요

--- 백준 문제풀기/파이썬/DFS와 BFS/algorithm_2206.py
+++ 백준 문제풀기/파이썬/DFS와 BFS/algorithm_2206.py
@@ -18,51 +18,32 @@ import sys
 from collections import deque
 n, m = map(int,sys.stdin.readline().split())
 maps = [list(map(int,list(sys.stdin.readline().rstrip()))) for _ in range(n)]
+visited = [[[False, False] for _ in range(m)] for _ in range(n)]
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-dp = [[False for _ in range(m)] for _ in range(n)]
-distance = [[0 for _ in range(m)] for _ in range(n)]
 
 def bfs():
     queue = deque()
-    queue.append((0,0))
-    distance[0][0] = 1
-
+    queue.append([0,0,0])
+    visited[0][0][0] = 1
     while queue:
-
-        x, y = queue.popleft()
-        isbroken = dp[x][y]
-
+        x, y, w = queue.popleft()
+        # w는 x,y가 벽 만남 여부 나타냄 0이면 안만남 1이면 만남.
         if x == n-1 and y == m -1:
-            break
+            return visited[x][y][w]
 
         for i in range(4):
             p = x + dx[i]
             q = y + dy[i]
 
-            if (0 <= p < n and 0 <= q < m and distance[p][q] == 0) or (0 <= p < n and 0 <= q < m and distance[p][q] >= distance[x][y] + 1 and isbroken):
-                if isbroken:
-                    if maps[p][q] == 0:
-                        queue.append((p,q))
-                        dp[p][q] = True
-                        distance[p][q] = distance[x][y] + 1
-                else:
-                    if maps[p][q] == 0:
-                        queue.append((p,q))
-                        dp[p][q] = False
-                        distance[p][q] = distance[x][y] + 1
-                    else:
-                        queue.append((p,q))
-                        dp[p][q] = True
-                        distance[p][q] = distance[x][y] + 1
+            if 0 <= p < n and 0 <= q < m :
+                if maps[p][q] == 0 and visited[p][q][w] == 0:
+                    visited[p][q][w] = visited[x][y][w] + 1
+                    queue.append([p,q,w])
+                elif maps[p][q] == 1 and w == 0:
+                    visited[p][q][1] = visited[x][y][0] + 1
+                    queue.append([p,q,1])
+    return -1
 
-
-    if distance[n-1][m-1] == 0:
-        print(-1)
-    else:
-        print(distance[n-1][m-1])
-
-    return
-
-bfs()
+print(bfs())
