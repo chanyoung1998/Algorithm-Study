9a3c18c98503a5f9ceb7f8a707d93067316c79cb
정찬영 <lms062111@naver.com>
2021-04-24 16:02:09 +0900

    21.4.23

--- 백준 문제풀기/파이썬/트리/algorithm_1991.py
+++ 백준 문제풀기/파이썬/트리/algorithm_1991.py
@@ -5,6 +5,49 @@
 '''
 import sys
 
+
 n = int(sys.stdin.readline())
-adjlist = [sys.stdin.readline().rstrip().split() for _ in range(n)]
-visited = [False for _ in range(n)]
+node = [[-1,-1] for _ in range(n+1)]
+for _ in range(n):
+    temp = list(map(ord,sys.stdin.readline().rstrip().split()))
+    number = temp[0] - 65
+    left = temp[1] - 65
+    right = temp[2] - 65
+    if left >= 0:
+        node[number][0] = left
+    if right >= 0:
+        node[number][1] = right
+
+def pretravel(num):
+
+    if num == -1:
+        return
+
+    print(chr(num+65),end='')
+    pretravel(node[num][0])
+    pretravel(node[num][1])
+
+def intravel(num):
+
+    if num == -1:
+        return
+
+    intravel(node[num][0])
+    print(chr(num+65),end='')
+    intravel(node[num][1])
+
+def posttravel(num):
+
+    if num == -1:
+        return
+
+    posttravel(node[num][0])
+    posttravel(node[num][1])
+    print(chr(num+65),end='')
+pretravel(0)
+print()
+intravel(0)
+print()
+posttravel(0)
+
+
