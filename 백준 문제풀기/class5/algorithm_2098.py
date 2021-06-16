33ccb7466b66365786b655c00f1a13ee3d20912f
정찬영 <lms062111@naver.com>
2021-06-17 00:01:46 +0900

    21.6.17

--- 백준 문제풀기/class5/algorithm_2098.py
+++ 백준 문제풀기/class5/algorithm_2098.py
@@ -1,44 +1,22 @@
 import sys
 n = int(sys.stdin.readline())
+dp = [[0] * (1 << n) for _ in range(n)]
+
 weight = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(n)]
-city = 16
 
-def inttobinary(number):
+for i in range(1,n):
+    if dp[i][0]:
+        dp[i][0] = weight[i][0]
+    else:
+        dp[i][0] = float('inf')
 
-    ret = []
-    while number > 0 :
-        if number % 2 == 0:
-            ret.append(0)
-        else:
-            ret.append(1)
-        number = number //2
-
-    ret = [0 for _ in range(n-len(ret))] + ret[::-1]
-
-    return ret
-
-def binarytoint(binary):
-    ret = 0
-    x = 1
-    for i in binary[::-1]:
-        ret += i * x
-        x *= 2
-
-    return ret
-
-def visit(visited,city):
-    visited_binary = inttobinary(visited)
-    visited_binary[city] = 1
-    return binarytoint(visited_binary)
-
-dp = [[0 for _ in range(2 **(n-1))] for _ in range(n)]
-
+# dp[curren][route] : current에서 출발하여 route에 있는 도시들을 거쳐 0번 노드로 돌아오는 최단거리
 def solution(current,route):
 
     if dp[current][route] != 0:
         return dp[current][route]
 
-    if route == (1 << (n-1)) -1:
+    if route == (1 <<(n-1)) - 1:
         if weight[current][0]:
             return weight[current][0]
         else:
@@ -48,12 +26,13 @@ def solution(current,route):
     for j in range(1,n):
         if not weight[current][j]:
             continue
-        if route & (1 << j - 1):
+        if route & (1 << j - 1):  # route에 j번 째 도시가 포함 되냐? 각 도시는 한 번씩만 포함되어야 하기 때문에 포함되면 안된다.
             continue
         dist = weight[current][j] + solution(j,route | (1 << (j-1)))
         if min_dist > dist:
             min_dist = dist
         dp[current][route] = min_dist
-        return min_dist
+
+    return min_dist
 
 print(solution(0,0))
