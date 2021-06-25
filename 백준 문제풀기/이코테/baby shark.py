0ff46ed18fab11f8864051683b02e88fd2ee2258
정찬영 <lms062111@naver.com>
2021-06-25 21:37:40 +0900

    21.6.25

---
+++ 백준 문제풀기/이코테/baby shark.py
@@ -0,0 +1,117 @@
+import sys
+import heapq
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+map = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(n)]
+
+shark_size = 2
+
+fish_list = [[] for _ in range(7)]
+cur_x = 0
+cur_y = 0
+for i in range(n):
+    for j in range(n):
+        if 1 <= map[i][j] <= 6:
+            fish_list[map[i][j]].append((i,j))
+        if map[i][j] == 9:
+            cur_x = i
+            cur_y = j
+
+map[cur_x][cur_y] = 0
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+
+def bfs(cur_x,cur_y):
+    queue = deque()
+    queue.append((cur_x,cur_y))
+    temp = [[-1 for _ in range(n)] for _ in range(n)]
+    temp[cur_x][cur_y] = 0
+    while queue:
+        p,q = queue.popleft()
+
+        for i in range(4):
+            new_p = dx[i] + p
+            new_q = dy[i] + q
+
+            if 0<= new_p < n and 0 <= new_q < n and map[new_p][new_q] <= shark_size and temp[new_p][new_q] == -1:
+                temp[new_p][new_q] = temp[p][q] + 1
+                queue.append((new_p,new_q))
+
+    return temp
+
+def bfs2(cur_x,cur_y,x,y):
+    queue = deque()
+    queue.append((cur_x,cur_y))
+    temp = [[-1 for _ in range(n)] for _ in range(n)]
+    temp[cur_x][cur_y] = 0
+    while queue:
+        p,q = queue.popleft()
+        if p == x and q ==y:
+            break
+        for i in range(4):
+            new_p = dx[i] + p
+            new_q = dy[i] + q
+
+            if 0 <= new_p < n and 0 <= new_q < n and map[new_p][new_q] <= shark_size and temp[new_p][new_q] == -1:
+                temp[new_p][new_q] = temp[p][q] + 1
+                queue.append((new_p,new_q))
+
+    return temp[x][y]
+
+
+ret = 0
+eaten = 0
+while True:
+
+    can_eat = 0
+    for i in range(1,shark_size):
+        if i <= 6:
+            can_eat += len(fish_list[i])
+        else:
+            break
+
+    if can_eat == 0:
+        break
+
+    '''if can_eat == 1:
+        x = 0
+        y = 0
+
+        for i in range(1, shark_size):
+            if i >= 7:
+                break
+            for (p, q) in fish_list[i]:
+                x,y = p,q
+                fish_list[i].remove((p,q))
+
+        dist = bfs2(cur_x, cur_y,x,y)
+        ret += dist
+        cur_x = x
+        cur_y = y
+    elif can_eat >= 2:'''
+    distances = []
+    dist_map = bfs(cur_x, cur_y)
+
+    for i in range(1,shark_size):
+        if i >= 7:
+            break
+        for (x,y) in fish_list[i]:
+            distances.append((x,y,dist_map[x][y],i))
+
+    distances.sort(key=lambda x:(x[2],x[0],x[1]))
+    #print("min",distances[0][2],"x",distances[0][0],"y",distances[0][1])
+    ret += distances[0][2]
+    fish_list[distances[0][3]].remove((distances[0][0],distances[0][1]))
+    cur_x = distances[0][0]
+    cur_y = distances[0][1]
+
+
+    eaten += 1
+    if eaten == shark_size:
+        shark_size += 1
+        eaten = 0
+
+print(ret)
--- 백준 문제풀기/이코테/final ranking.py
+++ 백준 문제풀기/이코테/final ranking.py
@@ -1,5 +1,6 @@
 import sys
-from itertools import combinations
+from collections import deque
+
 t = int(sys.stdin.readline())
 
 for _ in range(t):
@@ -7,24 +8,56 @@ for _ in range(t):
     prev_ranking = list(map(lambda x:x-1,map(int,sys.stdin.readline().rstrip().split())))
     m = int(sys.stdin.readline())
     changed_ranking = []
-    none_changed_ranking = []
+    graph = [[False] * n for _ in range(n)]
     indegree = [0 for _ in range(n)]
+
+    for i in range(n):
+        for j in range(i+1,n):
+            graph[prev_ranking[i]][prev_ranking[j]] = True
+            indegree[prev_ranking[j]] += 1
 
     for _ in range(m):
         a, b = map(lambda x:x-1,map(int,sys.stdin.readline().rstrip().split()))
         changed_ranking.append((a,b))
-        if prev_ranking.index(a) > prev_ranking.index(b):
-            indegree[b] += 1
-        else:
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
             indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[b][a] = False
+            graph[a][b] = True
+            indegree[b] += 1
+            indegree[a] -= 1
 
 
-    for x,y in combinations(range(n),2):
-        if (x,y) not in changed_ranking:
-            none_changed_ranking.append((x,y))
-            if prev_ranking.index(x) < prev_ranking.index(y):
-                indegree[y] += 1
-            else:
-                indegree[x] += 1
+    queue = deque()
+    for i in range(n):
+        if indegree[i] == 0:
+            queue.append(i)
 
+    cycle = False
+    certain = True
+    result = []
+    for i in range(n):
+        if len(queue) >= 2:
+            certain = False
+            break
+        if len(queue) == 0:
+            cycle = True
+            break
 
+        x = queue.popleft()
+        result.append(x+1)
+        for i in range(n):
+            if graph[x][i]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    queue.append(i)
+
+    if not certain:
+        print("?")
+    elif cycle:
+        print("IMPOSSIBLE")
+    else:
+        print(*result)
