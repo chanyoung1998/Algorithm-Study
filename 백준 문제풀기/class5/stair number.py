bfd90f2ade55ef61d7d95f40b59c47d2b0ece03e
정찬영 <lms062111@naver.com>
2021-06-29 15:15:43 +0900

    6.29 계단수 - 시간초과 ver

---
+++ 백준 문제풀기/class5/stair number.py
@@ -0,0 +1,43 @@
+import sys
+
+n = int(sys.stdin.readline())
+array = [-1 for _ in range(n)]
+IsIncluded = [[] for _ in range(10)]
+ret = 0
+def sol(num,count):
+    global ret
+
+    if num == 0 and count == n-1:
+        return
+
+
+    array[count] = num
+    IsIncluded[num].append(True)
+
+    if count == n-1:
+        flag = True
+        for temp in IsIncluded:
+            if True not in temp:
+                flag = False
+                break
+        if flag:
+            print(*array)
+            ret += 1
+    else:
+        if num == 0:
+            sol(1,count+1)
+
+        elif num == 9:
+            sol(8,count+1)
+        else:
+            sol(num+1,count+1)
+            sol(num-1,count+1)
+
+    array[count] = -1
+    IsIncluded[num].pop()
+    return
+
+
+for i in range(10):
+    sol(i,0)
+print(ret % 1000000000)
--- 백준 문제풀기/class5/vector matching_1007.py
+++ 백준 문제풀기/class5/vector matching_1007.py
@@ -1,8 +1,31 @@
 import sys
+from itertools import combinations
+def destination(x, y, p, q):
+    return ((x - p) ** 2 + (y - q) ** 2) ** 0.5
+
 t = int(sys.stdin.readline())
 for _ in range(t):
     n = int(sys.stdin.readline())
     vectors = []
+    check = [False for _ in range(n)]
     for _ in range(n):
         x,y = map(int,sys.stdin.readline().rstrip().split())
         vectors.append((x,y))
+
+    combination = list(combinations(range(n),n//2))
+    result = sys.maxsize
+    for comb in combination[:len(combination)//2]:
+        sum_x = 0
+        sum_y = 0
+        for i in range(n):
+            if i in comb:
+                sum_x += vectors[i][0]
+                sum_y += vectors[i][1]
+
+            else:
+                sum_x -= vectors[i][0]
+                sum_y -= vectors[i][1]
+
+        result = min(result,(sum_x**2 + sum_y**2)**0.5)
+
+    print(result)
