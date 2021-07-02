a68af2290c5d99f373b3a711d380bb9cfccb2be0
정찬영 <lms062111@naver.com>
2021-07-02 19:50:16 +0900

    21.7.2 단계별 풀기 동적 계획법 3 - 1311번

--- 백준 문제풀기/파이썬/동적계획법3(비트마스킹)/algorithm_1311.py
+++ 백준 문제풀기/파이썬/동적계획법3(비트마스킹)/algorithm_1311.py
@@ -2,18 +2,25 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 costs = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(n)]
+INF = sys.maxsize
+dp = [[-1 for _ in range(1 << n)] for _ in range(n)]
 
-dp = [[[0 for _ in range(1 << n)] for _ in range(n)] for _ in range(n)]
+def sol(x,visited):
 
-def sol():
-    full = (1 << n) - 1
+    if visited == (1 << n) - 1:
+        return 0
 
+    if dp[x][visited] != -1:
+        print(x,visited)
+        return dp[x][visited]
+
+    dp[x][visited] = INF
     for i in range(n):
-        dp[0][i][1<<i] = costs[0][i]
+        if visited & (1 << i):
+            continue
 
-    for person in range(1,n):
-        for job in range(n):
-            for bit in range(1<<n):
-                if bit & 1 << job == 1:
-                    continue
-                dp[person][job][bit| 1 << job] = min(dp[person][job][bit| 1 << job], )
+        dp[x][visited] = min(dp[x][visited],sol(x+1,visited | 1 << i) + costs[x][i])
+
+    return dp[x][visited]
+
+print(sol(0,0))
