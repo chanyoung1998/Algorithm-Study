0a3597de3c5863483b3d77eed7be3826d63cb011
정찬영 <lms062111@naver.com>
2021-08-01 22:17:52 +0900

    21.8.1 타일 채우기2

--- 백준 문제풀기/파이썬/동적계획법4/algorithm_13976.py
+++ 백준 문제풀기/파이썬/동적계획법4/algorithm_13976.py
@@ -1,10 +1,11 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-base = [[3,2],[1,0]]
+base = [[4,-1],[1,0]]
 mod = 1000000007
 
 def matrix_gop(a,b):
+
     answer = []
     for idx1 in range(len(a)):
         row = []
@@ -12,20 +13,24 @@ def matrix_gop(a,b):
             tmp = 0
             for idx3 in range(len(a[0])):
                 tmp += (a[idx1][idx3] * b[idx3][idx2]) % mod
-                tmp %= mod
+                tmp = (tmp+mod) % mod
             row.append(tmp)
         answer.append(row)
+
+
     return answer
 
 def matrix_power(a,x):
     res = [[1,0],[0,1]]
-    base = [[3,2],[1,0]]
     while x > 0:
         if x % 2 == 1:
-            res = matrix_gop(base,a)
-        x /= 2
+            res = matrix_gop(res,a)
         a = matrix_gop(a,a)
-
+        x = x // 2
     return res
 
-print(matrix_gop(matrix_power(base,n-4),[[3],[0]]))
+if n % 2 == 1:
+    print(0)
+else:
+    power = matrix_power(base,n//2)
+    print(matrix_gop(power,[[1,0],[1,0]])[0][0])
