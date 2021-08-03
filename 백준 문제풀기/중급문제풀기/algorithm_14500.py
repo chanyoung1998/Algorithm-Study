1c5b77dd44b6ddfb36a5fd3f4778f2dcee0c0759
정찬영 <lms062111@naver.com>
2021-08-04 00:03:04 +0900

    solve 테트로미노 14500

--- 백준 문제풀기/중급문제풀기/algorithm_14500.py
+++ 백준 문제풀기/중급문제풀기/algorithm_14500.py
@@ -6,11 +6,12 @@ visited = [[False for _ in range(m)] for _ in range(n)]
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
+
 def dfs(i,j,count,sum):
     global ret
 
     if count == 4:
-        print(sum)
+        #print(sum)
         ret = max(ret,sum)
         return
 
@@ -24,11 +25,38 @@ def dfs(i,j,count,sum):
             dfs(p,q,count+1,sum)
     visited[i][j] = False
 
+
+ex = [[0,0,0,1],[0,1,2,1],[0,0,0,-1],[0,-1,0,1]]
+ey = [[0,1,2,1],[0,0,0,1],[0,1,2,1],[0,1,1,1]]
+
+
+def check_fuck_shape(i,j):
+    global ret
+    for x in range(4):
+        isOut = False
+        sum_value = 0
+
+        for y in range(4):
+            p = i + ex[x][y]
+            q = j + ey[x][y]
+
+            if p < 0 or p >= n or q < 0 or q >= m:
+                isOut = True
+                break
+            else:
+                sum_value += weights[p][q]
+
+        if not isOut:
+            ret = max(ret,sum_value)
+    return
+
+
 max_value = 0
 for i in range(n):
     for j in range(m):
         ret = 0
         dfs(i,j,0,0)
+        check_fuck_shape(i,j)
         max_value = max(max_value,ret)
 print(max_value)
 
