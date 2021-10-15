c2fd81ce047effe2c9532323f827bb0c37e44948
정찬영 <lms062111@naver.com>
2021-10-15 22:11:48 +0900

    312

--- 2021인하대 프로그래밍 경진대회/1.py
+++ 2021인하대 프로그래밍 경진대회/1.py
@@ -9,52 +9,39 @@ for _ in range(m):
     adjlist[u].append((v,w))
 
 
-# 최단 경로와 목적지 까지 가는데 필요한 경로의 개수 구하기
 def dijkstra(start):
     queue = []
     dp[start] = 0
     path[start] = 0
     heapq.heappush(queue,(0,start))
+    ret = 0
 
     while queue:
         distance,current = heapq.heappop(queue)
         if dp[current] < distance:
             continue
-        for next, weight in adjlist[current]:
+        for next,weight in adjlist[current]:
             if dp[next] > dp[current] + weight:
+
+                if next == y:
+                    if dp[next] == dp[current] + weight and path[next] == path[current] + 1:
+                        ret += 1
+                    elif dp[next] == dp[current] + weight and path[next] > path[current] + 1:
+                        ret = 1
+                    elif dp[next] > dp[current] + weight:
+                        ret = 1
+
                 dp[next] = dp[current] + weight
                 path[next] = path[current] + 1
                 heapq.heappush(queue,(dp[next],next))
-    return
+
+    return ret
 
 
-def CountNode(start):
-
-    count = [0 for _ in range(n + 1)]
-    visit = [False for _ in range(n+1)]
-    count[start] = 1
-    queue = []
-    heapq.heappush(queue,start)
-
-    while queue:
-        cur = heapq.heappop(queue)
-        if visit[cur]:
-            continue
-        visit[cur] = True
-        for next,weight in adjlist[cur]:
-            if dp[cur] + weight == dp[next] and path[cur] + 1 == path[next]:
-                count[next] += count[cur]
-                count[next] %= 10 ** 9 + 9
-                heapq.heappush(queue,next)
-
-
-    #print(count)
-    return count[y] % (10**9 + 9)
-
-dijkstra(x)
+ret1 = dijkstra(x)
 if dp[y] == sys.maxsize:
     print(-1)
 else:
     print(dp[y])
     print(path[y])
-    print(CountNode(x))
+    print(ret1 % (10**9 + 9))
